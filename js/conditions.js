7cd16650c41be69f817cfbad140a9a188c790910
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-11-17 11:43:40 +0530

    loops and conditions

---
+++ js/conditions.js
@@ -0,0 +1,17 @@
+var a=19;
+var n="Ind"
+if(a>18){
+    if(n=="Indian"){
+        console.log("Eligible");
+    }
+    else{
+        console.log("not eligible because of nationality");
+        
+    }
+}
+else if(a==18){
+    console.log("Eligible for learning licence");
+}
+else{
+    console.log("Not Eligible");
+}
---
+++ js/loops.js
@@ -0,0 +1,18 @@
+// var a=0;  ---> variable initialisation
+// do{
+//     console.log(a); -->operation
+//     a++;            -->increment/decrement
+// }while (a<10) -->condition
+
+// var a=10;
+
+// while (a<10) {
+//     console.log(a);
+//     a++;
+// }
+
+for(var a=1; a<=100;a++){
+    if(a%3==0 && a%5==0){
+        console.log(a);
+    }
+}
