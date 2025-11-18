b2e21b953ca98e9a4e8e39f215d1f7fe3cb10455
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-11-18 11:32:49 +0530

    array and objercts

---
+++ js/array.js
@@ -0,0 +1,23 @@
+var a=[24,46,345,343,25,"234"]
+// var a=[0, 1,  2,  3,  4,  5]
+
+// console.log(a[0]);
+
+// console.log(a.length);
+
+// for(var i=0;i<a.length;i++){
+//     console.log(a[i]);   
+// }
+
+// console.log("Before push",a);
+// a.push("Hello");
+// console.log("After push",a);
+// a.pop();
+// console.log("After pop",a);
+
+
+console.log("Before shift",a);
+a.shift();
+console.log("After shift",a);
+a.unshift("Hello");
+console.log("After unshift",a);
---
+++ js/object.js
@@ -0,0 +1,29 @@
+// var obj={
+//     stu_name:"Rahul",
+//     rollno:34
+// }
+
+
+// console.log("Before Changing");
+
+// console.log(obj.stu_name);
+// console.log(obj.rollno);
+
+// console.log("After Changing");
+// obj.stu_name="Ramesh";
+// obj.rollno=44;
+
+// console.log(obj.stu_name);
+// console.log(obj.rollno);
+
+
+// Array of onbjects
+
+var stu_data=[
+    { stu_name:"Rahul",rollno:34},
+    { stu_name:"Ramesh",rollno:34},
+    { stu_name:"Suresh",rollno:34},
+    { stu_name:"Kalpesh",rollno:34}
+]
+
+console.log(stu_data[2].stu_name);
